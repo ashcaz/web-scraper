22b780a530ca9f4509263ba60c7c41ae38fdd1b2
Ashley Casimir <ashcas@me.com>
2021-02-04 21:50:42 -0800

    get citiations needed count complete tests pass

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,5 +1,24 @@
+import pytest
+import validators
+from web_scraper.web_scraper import get_citations_needed_count
 from web_scraper import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+# @pytest.mark.skip("pending")
+def test_get_citations_bad_url():
+    with pytest.raises(Exception) as err:
+        get_citations_needed_count("https://badurl")
+
+        assert str(err.value) == "Not a valid URL, Try again"
+
+
+def test_get_citations_good_url():
+    actual = get_citations_needed_count(
+        "https://en.wikipedia.org/wiki/History_of_Haiti"
+    )
+    expected = 3
+    assert actual == expected
--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,8 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import validators
 
-url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+# url = "https://en.wikipedia.org/wiki/History_of_Haiti"
 
-response = requests.get(url)
-content = response.content
-print(content)
+# response = requests.get(url)
+
+# content = response.content
+
+# # print(content)
+
+# soup = BeautifulSoup(content, "html.parser")
+# result = soup.find_all("a", text="citation needed")
+# print(result)
+# print(len(result))
+
+
+def scraped_results(url):
+    pass
+
+
+def get_citations_needed_count(url):
+
+    if not validators.url(url):
+        raise Exception("Not a valid URL, Try again")
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    result = soup.find_all("a", text="citation needed")
+    citations_needed = len(result)
+    return citations_needed
