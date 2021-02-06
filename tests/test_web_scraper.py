64e14da088abcdd6f34dc69a8a25f92eabd2e10e
Ashley Casimir <ashcas@me.com>
2021-02-05 21:03:10 -0800

    added tests and invoked functions in main function

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -20,8 +20,32 @@ def test_scraped_results_bad_url():
         assert str(err.value) == "Not a valid URL, Try again"
 
 
+# # @pytest.mark.skip("pending")
+# def test_scraped_results_good_url(capsys):
+#     scraped_results("https://en.wikipedia.org/wiki/History_of_Haiti")
+#     captured = capsys.readouterr()
+#     assert captured.out == "URL is valid\n"
+
+
 # @pytest.mark.skip("pending")
-def test_scraped_results_good_url(capsys):
-    scraped_results("https://en.wikipedia.org/wiki/History_of_Haiti")
-    captured = capsys.readouterr()
-    assert captured.out == "URL is valid\n"
+def test_get_citations_needed():
+    scraped = scraped_results("https://en.wikipedia.org/wiki/History_of_Haiti")
+    actual = get_citations_needed_count(scraped)
+    expected = 3
+    assert actual == expected
+
+
+# @pytest.mark.skip("pending")
+def test_check_citations_report():
+    scraped = scraped_results("https://en.wikipedia.org/wiki/History_of_Haiti")
+    actual = get_citations_needed_count(scraped)
+    expected = 3
+    assert actual == expected
+
+
+# @pytest.mark.skip("pending")
+def test_report():
+    scraped = scraped_results("https://en.wikipedia.org/wiki/History_of_Haiti")
+    actual = get_citations_needed_report(scraped)
+
+    assert type(actual) == str
--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import validators
 
-# url = "https://en.wikipedia.org/wiki/History_of_Haiti"
+url = "https://en.wikipedia.org/wiki/History_of_Haiti"
 
 # response = requests.get(url)
 
@@ -19,8 +19,6 @@ import validators
 def scraped_results(url):
     if not validators.url(url):
         raise Exception("Not a valid URL, Try again")
-    else:
-        print("URL is valid")
 
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -38,10 +36,22 @@ def get_citations_needed_count(scraped_results):
 
 def get_citations_needed_report(scraped_results):
 
+    report_list = []
+
     for citation in scraped_results:
 
         text = citation.find_parent("p").text
-        print(text)
+        report_list.append(text)
+
+    return "\n\n".join(report_list)
 
 
-# if __name__ == '__main__':
+if __name__ == "__main__":
+
+    scraped_info = scraped_results(url)
+
+    count = get_citations_needed_count(scraped_info)
+
+    report = get_citations_needed_report(scraped_info)
+
+    print(f"\nCitations Count: {count} \n\nCitations Report:\n\n{report} ")
